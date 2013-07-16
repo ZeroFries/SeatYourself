91f0998bdb6067909c68152b95de0c5dc1a48d7a
Stuart Garvagh <sgarvagh@uoguelph.ca>
2013-07-16 12:56:18 -0400

    setting up dev environment

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Seatyourself::Application.config.secret_key_base = '5a92abee3b08352edac9a98481bab3d9627d1250884ac3b65e1d90135fe782419c1b02fd7c2aa9fb911387580e093807faab572f7d15c27f25a516d271da715e'
